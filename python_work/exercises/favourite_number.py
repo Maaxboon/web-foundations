b8637639c6c171beb37fa102104a46b789139bde
Maaxboon <maaxwelokoth@gmail.com>
2025-07-16 06:44:29 +0300

    Favourite number

---
+++ python_work/exercises/favourite_number.py
@@ -0,0 +1,8 @@
+"""
+2-9. Favorite Number: Store your favorite number in a variable . Then, using 
+that variable, create a message that reveals your favorite number . Print that 
+message
+"""
+favourite_number = 4
+message = "My favourite number is " + str(favourite_number)
+print(message)
