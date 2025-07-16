9586284b861c14b9d1c5abe5923d46c48ce6e8da
Maaxboon <maaxwelokoth@gmail.com>
2025-07-16 06:42:16 +0300

    Playing around with number 8

---
+++ python_work/exercises/number_eight.py
@@ -0,0 +1,20 @@
+"""2-8. Number Eight: Write addition, subtraction, multiplication, and division 
+operations that each result in the number 8 . Be sure to enclose your operations 
+in print statements to see the results . You should create four lines that look 
+like this:
+ print(5 + 3)
+ Your output should simply be four lines with the number 8 appearing once 
+on each line .
+"""
+# Addition
+print(3 + 5)
+
+# subtraction
+print(9 - 1)
+
+# multiplication
+print(4 * 2)
+
+# division
+print(int(40 / 5))
+  
