eab7bb102ff70e6092e913513aadc3109bad3995
Maaxboon <maaxwelokoth@gmail.com>
2025-07-17 07:53:01 +0300

    Accessing and using the elements in a list

---
+++ python_work/exercises/chapter_3/cars.py
@@ -0,0 +1,13 @@
+"""
+Your Own List: Think of your favorite mode of transportation, such as a 
+motorcycle or a car, and make a list that stores several examples . Use your list 
+to print a series of statements about these items, such as “I would like to own a 
+Honda motorcycle .
+"""
+dream_cars = ['Axela', 'GTI', 'B 180', '308 GT', 'X3 M Drive']
+
+print("I love the Mazda " + dream_cars[0] + ".")
+print("A Golf " + dream_cars[1] + " is my go to for performance.")
+print("We live in a space where the " + dream_cars[2] + " is driven as a fist car.")
+print("Not many people know of how good the Peugeot " + dream_cars[-2] + " is.")
+print(dream_cars[-1] + " offers comfort, luxury and performance.")
