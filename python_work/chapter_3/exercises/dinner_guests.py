4d3fefcaf57a49a7163c7a69e48e621539e17460
Maaxboon <maaxwelokoth@gmail.com>
2025-07-24 07:02:03 +0300

    Finding the length of a list

---
+++ python_work/chapter_3/exercises/dinner_guests.py
@@ -0,0 +1,9 @@
+"""
+3-9. Dinner Guests: Working with one of the programs from Exercises 3-4 
+through 3-7 (page 46), use len() to print a message indicating the number 
+of people you are inviting to dinner 
+"""
+guests = ['Messi', 'Linus', 'Curry']
+message = "I am inviting " + str(len(guests)) + " guests to the dinner."
+
+print(message)
