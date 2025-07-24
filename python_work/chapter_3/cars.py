f7079a1575f88434b5e6ffd95d9dd79e4324b9d6
Maaxboon <maaxwelokoth@gmail.com>
2025-07-24 06:06:41 +0300

    Sorting a List Permanently with the sort() Method

---
+++ python_work/chapter_3/cars.py
@@ -0,0 +1,7 @@
+# Sorting a List Permanently with the sort() Method
+"""
+Python’s sort() method makes it relatively easy to sort a list. Imagine we have a list of cars and want to change the order of the list to store them alphabetically. To keep the task simple, let’s assume that all the values in the list are lowercase.
+"""
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
