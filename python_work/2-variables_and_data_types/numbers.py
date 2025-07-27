eeda8f52e9894506c8cb5f071de3a3283752fcfa
Maaxboon <maaxwelokoth@gmail.com>
2025-07-27 21:28:13 +0300

    variables and data types

---
+++ python_work/2-variables_and_data_types/numbers.py
@@ -0,0 +1,34 @@
+# Integers
+# You can add (+), subtract (-), multiply (*), and divide (/) integers in Python.
+sum = 3 + 2
+print(sum)
+
+difference = 3 - 2
+print(difference)
+
+product = 3 * 2
+print(product)
+
+division = 3 / 2
+print(division)
+
+# Python uses two multiplication symbols to represent exponents:
+square_of_3 = 3 ** 2
+print(square_of_3)
+
+# Modulo used to find the remainder
+remainder = 3 % 2
+print(remainder)
+
+# type: ignore # Floats: Python calls any number with a decimal point a float.
+decimal_sum = 2.1 + 0.1
+print(decimal_sum)
+
+decimal_difference = 2.1 - 0.1
+print(decimal_difference)
+
+decimal_product = 2.1 * 0.1
+print(decimal_product)
+
+decimal_division = 2.1 / 0.1
+print(decimal_division)
