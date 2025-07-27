7b50ab415b20c33223e0b73ee2ae468f7513546f
Maaxboon <maaxwelokoth@gmail.com>
2025-07-27 21:27:44 +0300

    Basics

---
+++ python_work/1-getting_started/exercises/simple_messages.py
@@ -0,0 +1,5 @@
+campus = "The Technical University of kenya" # creating a variable campus and storing the value "The Technical University of kenya"
+print(campus) # prints: The Technical University of kenya
+
+campus = "African Leadership University" # changes the campus value to: African Leadership University
+print(campus) # prints: African Leadership University
