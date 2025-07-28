8ef55fc2dcdbf4dffe3693030581828aa08ef4fe
Maaxboon <maaxwelokoth@gmail.com>
2025-07-28 10:22:17 +0300

    Adds comments to explain the code

--- python_work/4-working_with_lists/magicians.py
+++ python_work/4-working_with_lists/magicians.py
@@ -1,3 +1,3 @@
-magicians = ['alice', 'david', 'carolina']
-for magician in magicians:
-    print(magician)
+magicians = ['alice', 'david', 'carolina'] # We begin by defining a list
+for magician in magicians: # This line tells Python to pull a name from the list magicians, and store it in the variable magician
+    print(magician) #  we tell Python to print the name that was just stored in magician
