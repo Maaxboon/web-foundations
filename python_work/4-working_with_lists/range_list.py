d07a7b969d323ef4dfe5b7670f192a44e9ea503a
Maaxboon <maaxwelokoth@gmail.com>
2025-07-28 21:34:52 +0300

    Using range to make a list of numbers

---
+++ python_work/4-working_with_lists/range_list.py
@@ -0,0 +1,6 @@
+#  Using range() to Make a List of Numbers
+"""
+If you want to make a list of numbers, you can convert the results of range() directly into a list using the list() function. When you wrap list() around a call to the range() function, the output will be a list of numbers.
+"""
+numbers = list(range(1,6))
+print(numbers)
