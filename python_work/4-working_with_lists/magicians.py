76a5b79c8dd7a4345f0915e7086f5b911eae7464
Maaxboon <maaxwelokoth@gmail.com>
2025-07-28 20:39:04 +0300

    Working with indentation

--- python_work/4-working_with_lists/magicians.py
+++ python_work/4-working_with_lists/magicians.py
@@ -1,3 +1,10 @@
 magicians = ['alice', 'david', 'carolina'] # We begin by defining a list
 for magician in magicians: # This line tells Python to pull a name from the list magicians, and store it in the variable magician
-    print(magician) #  we tell Python to print the name that was just stored in magician
+    # print("\n" + magician) #  we tell Python to print the name that was just stored in magician
+
+# Doing more work within a for loop
+    print(magician.title() + ", that was a great trick!")
+    print("I can't wait to see your next trick " + magician.title() + ".\n")
+
+# Doing something after a for loop
+print("Thank you everyone. That was a great magic show.")
