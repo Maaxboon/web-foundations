1c9ab68756f5287d26fbd2c78f709c4541f23681
Maaxboon <maaxwelokoth@gmail.com>
2025-07-28 21:38:57 +0300

    Prints even numbers in a list using range

---
+++ python_work/4-working_with_lists/even_numbers.py
@@ -0,0 +1,6 @@
+"""
+We can also use the range() function to tell Python to skip numbers in a given range. For example, here’s how we would list the even numbers between 1 and 10:
+"""
+
+even_numbers = list(range(0,11,2)) # starts at 0 to eleven skipping odd numbers
+print(even_numbers)
