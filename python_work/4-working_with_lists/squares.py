76aed2c59277df77020487e56983538ddfac10fa
Maaxboon <maaxwelokoth@gmail.com>
2025-07-29 09:20:48 +0300

    Creating an empty list of squares from range

--- python_work/4-working_with_lists/squares.py
+++ python_work/4-working_with_lists/squares.py
@@ -18,21 +18,3 @@ for value in range(1,11):
 print("The squares of numbers between 0 and 11 are:")
 print(squares)
 
-# We start with an empty list called cubes
-cubes = []
-
-# We tell python to loop through each value from 1 to 5
-for i in range(1,6):
-
-    # Inside the loop, the current value is raised to the third power and stored in the variable cube
-    cube = i ** 3
-
-    # each new value of the cube is appended to the list cubes
-    cubes.append(cube)
-
-# To write this code more concisely, omit the temporary variable cube and append each new value directly to the list:
-# squares.append(value ** 3)
-
-# Finally, when the loop has finished running, the list of cubes is printed
-print("The cubes of numbers between 0 and 6 are:")
-print(cubes)
