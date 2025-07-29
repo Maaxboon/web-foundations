f0fe2fd3841b3bf4748da7db691ed92ec54fcf96
Maaxboon <maaxwelokoth@gmail.com>
2025-07-29 21:51:26 +0300

    Cube comprehension to generate a list of cubes using range

---
+++ python_work/4-working_with_lists/exercises/cube_comprehension.py
@@ -0,0 +1,9 @@
+"""
+ 4-9. Cube Comprehension: Use a list comprehension to generate a list of the first 10 cubes
+"""
+
+# List comprehension for cubes
+cubes = [value ** 3 for value in range(1,11)]
+
+# print the cubes
+print(cubes)
