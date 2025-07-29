4646681ef39997c00c2ad9c6a67aa802337922dc
Maaxboon <maaxwelokoth@gmail.com>
2025-07-29 22:18:26 +0300

    Making a list of the multiples of 3 from 3 to 30 using  a for loop

---
+++ python_work/4-working_with_lists/exercises/threes.py
@@ -0,0 +1,9 @@
+"""
+ 4-7. Threes: Make a list of the multiples of 3 from 3 to 30 . Use a for loop to print the numbers in your list .
+"""
+# Printing the multiples of three using a step function argument in the range
+multiples = []
+
+for value in range(3,31,3):
+    multiples.append(value)
+print(multiples)
