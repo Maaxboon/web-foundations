675c09b162ba997bafff4256d6d401c96537902e
Maaxboon <maaxwelokoth@gmail.com>
2025-07-29 21:29:47 +0300

    Using list comprehension to generate lists

--- python_work/4-working_with_lists/cubes.py
+++ python_work/4-working_with_lists/cubes.py
@@ -16,3 +16,7 @@ for i in range(1,6):
 # Finally, when the loop has finished running, the list of cubes is printed
 print("The cubes of numbers between 0 and 6 are:")
 print(cubes)
+
+# List comprehension: Allows us to generate the same listof cubes in just one line
+cubes = [value **3 for value in range(0,6)]
+print(cubes)
--- python_work/4-working_with_lists/squares.py
+++ python_work/4-working_with_lists/squares.py
@@ -18,3 +18,7 @@ for value in range(1,11):
 print("The squares of numbers between 0 and 11 are:")
 print(squares)
 
+# List comprehension: This allows us to generate the same list of squares in just one line of code
+squares = [value ** 2 for value in range(1,11)]
+print(squares)
+
