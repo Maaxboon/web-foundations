2a48de013839fe2556da0fb4fc12e396d03bce8b
Maaxboon <maaxwelokoth@gmail.com>
2025-07-30 11:25:47 +0300

    Boolean Expressions

--- python_work/5-if_statements/banned_users.py
+++ python_work/5-if_statements/banned_users.py
@@ -11,3 +11,14 @@ if user not in banned_users:
     print(user.title() + ", you can post a response if you wish")
 else:
     print(user.title() + ", you have been banned from posting a comment!")
+
+# Boolean Expressions
+"""
+As you learn more about programming, you’ll hear the term Boolean expression at some point. A Boolean expression is just another name for a conditional test. A Boolean value is either True or False, just like the value of a conditional expression after it has been evaluated.
+Boolean values are often used to keep track of certain conditions, such as whether a game is running or whether a user can edit certain content on a website:
+
+game_active = True
+can-edit = False
+
+Boolean values provide an efficient way to track the state of a program or a particular condition that is important in your program.
+"""
