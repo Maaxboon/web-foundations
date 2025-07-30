6408b8dda52914776120eb87f873aca098781ee3
Maaxboon <maaxwelokoth@gmail.com>
2025-07-30 11:13:42 +0300

    Checking Whether a Value Is in a List

--- python_work/5-if_statements/toppings.py
+++ python_work/5-if_statements/toppings.py
@@ -13,4 +13,26 @@ if requested_topping != 'anchovies':
     # Because the value of requested_topping is not 'anchovies', the print statement is executed:
     print("Hold the anchovies!")
 
-# Note:  Most of the conditional expressions you write will test for equality, but sometimes you’ll find it more efficient to test for inequality.s
+# Note:  Most of the conditional expressions you write will test for equality, but sometimes you’ll find it more efficient to test for inequality
+
+# Checking Whether a Value is in a List
+"""
+Sometimes it’s important to check whether a list contains a certain value before taking an action. For example, you might want to check whether a new username already exists in a list of current usernames before completing someone’s registration on a website. In a mapping project, you might want to check whether a submitted location already exists in a list of known 
+locations.
+To find out whether a particular value is already in a list, use the keyword in. Let’s consider some code you might write for a pizzeria. We’ll make a list of toppings a customer has requested for a pizza and then check whether certain toppings are in the list.
+"""
+
+# Checking Whether a Value is in a List
+"""
+Sometimes it’s important to check whether a list contains a certain value before taking an action. For example, you might want to check whether a new username already exists in a list of current usernames before completing someone’s registration on a website. In a mapping project, you might want to check whether a submitted location already exists in a list of known 
+locations.
+To find out whether a particular value is already in a list, use the keyword in. Let’s consider some code you might write for a pizzeria. We’ll make a list of toppings a customer has requested for a pizza and then check whether certain toppings are in the list.
+"""
+
+requested_toppings = ['mushrooms', 'onions', 'pineapple']
+print('mushrooms' in requested_toppings)
+print('pepperoni' in requested_toppings)
+
+"""
+the keyword in tells Python to check for the existence of 'mushrooms' and 'pepperoni' in the list requested_toppings. This technique is quite powerful because you can create a list of essential values, and then easily check whether the value you’re testing matches one of the values in the list.
+"""
