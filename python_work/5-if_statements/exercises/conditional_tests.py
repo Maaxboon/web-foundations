953b8ce53ce4a542859c15887129fdafaaccab49
Maaxboon <maaxwelokoth@gmail.com>
2025-07-30 11:49:31 +0300

    Conditional tests

---
+++ python_work/5-if_statements/exercises/conditional_tests.py
@@ -0,0 +1,85 @@
+"""
+5-1. Conditional Tests: Write a series of conditional tests . Print a statement describing each test and your prediction for the results of each test . Your code should look something like this:
+ car = 'subaru'
+ print("Is car == 'subaru'? I predict True.")
+ print(car == 'subaru')
+ print("\nIs car == 'audi'? I predict False.")
+ print(car == 'audi')
+ •	Look closely at your results, and make sure you understand why each line evaluates to True or False .
+ •	Create at least 10 tests . Have at least 5 tests evaluate to True and another 
+5 tests evaluate to False .
+"""
+# assign the value subaru to variable car
+car = 'subaru'
+
+# Check one
+# My prediction
+print("Is car == 'bmw'? I predict False. ")
+# Check
+print(car == 'subaru')
+
+# Check two
+# My prediction
+print("\nIs car == 'bmw'? I predict True")
+# Check
+print(car == 'bmw')
+
+# assign the value axela to variable car
+car = 'axela'
+
+# Check three
+# My prediction
+print("Is car == 'peugeot 308'? I predict False. ")
+# Check
+print(car == 'axela')
+
+# Check four
+# My prediction
+print("\nIs car == 'axela'? I predict True")
+# Check
+print(car == 'axela')
+
+# assign the value macan to variable car
+car = 'macan'
+
+# Check five
+# My prediction
+print("Is car == 'cayene'? I predict False. ")
+# Check
+print(car == 'macan')
+
+# Check six
+# My prediction
+print("\nIs car == 'macan'? I predict True")
+# Check
+print(car == 'macan')
+
+# assign the value tiguan to variable car
+car = 'tiguan'
+
+# Check seven
+# My prediction
+print("Is car == 'toureg'? I predict False. ")
+# Check
+print(car == 'tiguan')
+
+# Check eight
+# My prediction
+print("\nIs car == 'tiguan'? I predict True")
+# Check
+print(car == 'tiguan')
+
+# assign the value bentley to variable car
+car = 'bentley'
+
+# Check nine
+# My prediction
+print("Is car == 'limo'? I predict False. ")
+# Check
+print(car == 'bentley')
+
+# Check ten
+# My prediction
+print("\nIs car == 'bentley'? I predict True")
+# Check
+print(car == 'bentley')
