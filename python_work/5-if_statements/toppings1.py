428af92b27f3aef052ed9c6f734f84a7a22b0921
Maaxboon <maaxwelokoth@gmail.com>
2025-08-06 11:27:24 +0300

    Testing Multiple Conditions

---
+++ python_work/5-if_statements/toppings1.py
@@ -0,0 +1,16 @@
+#  list containing the requested toppings
+requested_toppings = ['mushrooms', 'extra cheese'] 
+
+# checks to see whether the person requested mushrooms on their pizza
+if 'mushrooms' in requested_toppings: 
+    print("Adding mushrooms.")
+
+# checks to see whether the person requested pepperoni on their pizza
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+
+# checks to see whether the person requested extra cheese on their pizza
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza!")
