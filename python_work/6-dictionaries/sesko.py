e3e810eeb8cc6061395a5fbb3c7e493bc1a7ad73
Maaxboon <maaxwelokoth@gmail.com>
2025-08-07 07:06:48 +0300

    Modifying values in a dictionary

---
+++ python_work/6-dictionaries/sesko.py
@@ -0,0 +1,11 @@
+# Modifying values in a dictionary
+"""
+To modify a value in a dictionary, give the name of the dictionary with the key in square brackets and then the new value you want associated with that key.
+"""
+
+sesko = {'team': 'leipzig', 'color': 'white'}
+print("\nSesko former team: " + str(sesko))
+
+sesko['team'] = 'mauntd'
+sesko['color'] = 'red'
+print("\nSesko new team: " + str(sesko))
