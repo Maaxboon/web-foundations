df1b0e32bb92508f5d4fb0b83d9ff9ca1db8c9fd
Maaxboon <maaxwelokoth@gmail.com>
2025-08-07 07:42:29 +0300

    Lionel Messi details in a dictionary

---
+++ python_work/6-dictionaries/exercises/person.py
@@ -0,0 +1,15 @@
+"""
+ 6-1. Person: Use a dictionary to store information about a person you know . 
+Store their first name, last name, age, and the city in which they live . You should have keys such as first_name, last_name, age, and city . Print each piece of information stored in your dictionary . 
+"""
+person = {
+    'first_name' : 'Lionel',
+    'last_name' : 'Messi',
+    'age' : 38,
+    'city' : 'Miami',
+}
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
