826ffaa58037c61994e373d9c73bb4af6ee7d524
Maxtoshi <maaxwelokoth@gmail.com>
2025-08-10 23:17:53 +0300

    Loops through a dictionary of people and their favorite places

---
+++ python_work/6-dictionaries/exercises/favorite_places.py
@@ -0,0 +1,10 @@
+"""
+6-9. Favorite Places: Make a dictionary called favorite_places. Think of three names to use as keys in the dictionary, and store one to three favorite places for each person. To make this exercise a bit more interesting, ask some friends to name a few of their favorite places. Loop through the dictionary, and print each person’s name and their favorite places.
+"""
+favorite_places = {
+    'maxtoshi': 'durban',
+    'poisedon': 'kitale',
+    'phil': 'bumbogo',
+}
+for name, place in favorite_places.items():
+    print(f"{name.title()}'s favorite place is {place.title()}")
