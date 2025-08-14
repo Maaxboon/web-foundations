0f5aa4962e9ce04330b1849d4dc15097dfe7f000
Maxtoshi <maaxwelokoth@gmail.com>
2025-08-14 10:30:42 +0300

    A simple input program for checking availability of rental cars

---
+++ python_work/7-user_inpunts_and_while_loops/exercises/rental_car.py
@@ -0,0 +1,7 @@
+"""
+7-1. Rental Car: Write a program that asks the user what kind of rental car they would like. Print a message about that car, such as “Let me see if I can find you a Subaru.”
+"""
+prompt = "What type of car do you want to rent? "
+car = input(prompt)
+
+print(f"A {car} is available for rental at 5000 KES per day.")
