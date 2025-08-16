95aed16b581a98d3594a0882b0a30b5863766029
Maxtoshi <maaxwelokoth@gmail.com>
2025-08-16 07:03:44 +0300

    An infinity loop

---
+++ python_work/7-user_inpunts_and_while_loops/exercises/infinity.py
@@ -0,0 +1,8 @@
+"""
+7-7. Infinity: Write a loop that never ends, and run it. (To end the loop, press 
+ctrl-C or close the window displaying the output.)
+"""
+x = 7
+while x >= 7:
+    print(x)
+    # x += 1
